274c39ccb76d2aacda689398685d14544e616562
antran998 <baoan11111@gmail.com>
2021-04-10 20:17:53 +0700

    add subjectId

--- seeders/category.js
+++ seeders/category.js
@@ -1,8 +1,8 @@
 const categorySeeding = (db) => {
   return db.category.bulkCreate([
-    { name: "Chương 1" },
-    { name: "Chương 2" },
-    { name: "Chương 3" },
+    { name: "Chương 1", subjectId: 1 },
+    { name: "Chương 2", subjectId: 1 },
+    { name: "Chương 3", subjectId: 1 },
   ]);
 };
 
